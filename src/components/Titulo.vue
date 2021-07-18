8cba43874b684b4ca1b1e84bc341255ba134fdb6
Enzo <enzod98@hotmail.com>
2021-07-18 12:55:06 -0400

    Commit inicial

---
+++ src/components/Titulo.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1
+  :style="{'color': colorFont}"
+  >Contador: {{ signoDolar }} </h1>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+    props: ['contador', 'colorFont'],
+    // computed: {
+    //     signoDolar() {
+    //         return '$ ' + this.contador
+    //     }
+    // },
+    setup(props){
+        const signoDolar = computed(() => {
+            return '$' + props.contador
+        })
+
+        return {signoDolar}
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <titulo :contador="contador" :colorFont="colorFont" />
+    <!-- <h1
+      :style="{'color': colorFont}"
+    >
+      Contador: {{ contador }}
+    </h1> -->
+    <button @click="aumentar">+</button>
+    <button @click="disminuir">-</button>
+    <hr>
+    <input type="text" v-model="texto">
+    <p>{{ texto }}</p>
   </div>
 </template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import { computed } from '@vue/runtime-core';
+import Titulo from '../components/Titulo.vue'
+export default {
+  components:{
+    Titulo
+  },
+  setup() {
+    const contador = ref(0);
+    const texto = ref('');
+
+    const aumentar = () => {
+      contador.value ++;
+    }
+
+    const disminuir = () => {
+      contador.value --;
+    }
+
+    const colorFont = computed(() => {
+      return contador.value > 0 ? 'green' : 'red'  
+    })
+
+    return { contador, aumentar, disminuir, colorFont, texto }
+  },
+}
+</script>
+
